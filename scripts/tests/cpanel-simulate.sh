e65dc31f50bcc867650099b48657b169a8be143d
Kenric Ashe <26514157+kenricashe@users.noreply.github.com>
2025-09-18 13:37:47 -0700

    fix: add -f flag to rm command when removing sites file in cpanel simulation

--- scripts/tests/cpanel-simulate.sh
+++ scripts/tests/cpanel-simulate.sh
@@ -282,7 +282,7 @@ case "${1:-}" in
 	"off")
 		remove_dummy_files
 		enable_CONF_DIR
-		[ -f "$SITES_FILE" ] && rm "$SITES_FILE"
+		[ -f "$SITES_FILE" ] && rm -f "$SITES_FILE"
 		echo ""
 		echo "✓ cPanel simulation DISABLED"
 		;;
