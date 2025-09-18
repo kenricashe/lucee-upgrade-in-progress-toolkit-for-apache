74b2673112bffc0260bdecd9601e28114a7a2e02
Kenric Ashe <26514157+kenricashe@users.noreply.github.com>
2025-09-17 20:41:25 -0700

    chore: update version timestamp to 2025-09-17 20:41:15 Pacific

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,5 +3,5 @@
 # Centralized version information for upgrade-in-progress scripts
 # UPDATE THIS WITH EACH COMMIT
 
-VERSION_DATE="2025-09-17 16:30:46 Pacific"
+VERSION_DATE="2025-09-17 20:41:15 Pacific"
 VERSION_STRING="Version: ${VERSION_DATE}"
