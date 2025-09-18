7469f3c628d046885e83fe90e5f22cecedd09585
Kenric Ashe <26514157+kenricashe@users.noreply.github.com>
2025-09-17 21:04:18 -0700

    feat: add permission matching for upgrade HTML file to match docroot

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,5 +3,5 @@
 # Centralized version information for upgrade-in-progress scripts
 # UPDATE THIS WITH EACH COMMIT
 
-VERSION_DATE="2025-09-17 20:48:25 Pacific"
+VERSION_DATE="2025-09-17 21:03:55 Pacific"
 VERSION_STRING="Version: ${VERSION_DATE}"
